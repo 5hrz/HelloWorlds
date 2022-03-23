aa5c9b370c347ad3c1f2a7830a3d9b0e8cefe8aa
5hrz <shrz1xyz@gmail.com>
2022-03-23 17:44:33 +0900

    [Add] Input file

--- src/bash/run.sh
+++ src/bash/run.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-for class in $(ls *.c);
+for class in $(ls *.bash);
 do
   if [ -f $class ]; then
-    echo "$ gcc ${class} -o ${class}.out" >> ${class}.log
-    gcc $class -o ${class}.out >> ${class}.log
-        if [ -e ${class}.in ]; then
+    if [ -e ${class}.in ]; then
         echo "$ ./${class} < ${class}.in" >> ${class}.log
         ./${class} < ${class}.in >> ${class}.log
     else
--- src/cpp/run.sh
+++ src/cpp/run.sh
@@ -5,7 +5,12 @@ do
   if [ -f $class ]; then
     echo "$ g++ ${class} -o ${class}.out" >> ${class}.log
     g++ $class -o ${class}.out >> ${class}.log
-    echo "$ ./${class}.out" >> ${class}.log
-    ./${class}.out >> ${class}.log
+    if [ -e ${class}.in ]; then
+        echo "$ ./${class} < ${class}.in" >> ${class}.log
+        ./${class} < ${class}.in >> ${class}.log
+    else
+        echo "$ ./${class}" >> ${class}.log
+        ./$class >> ${class}.log
+    fi
   fi
 done
