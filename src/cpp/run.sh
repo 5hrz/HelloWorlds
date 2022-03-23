84decef9188611ab45efb5493828d7779e29638f
5hrz <shrz1xyz@gmail.com>
2022-03-23 15:49:00 +0900

    [Fix] bash

--- src/cpp/run.sh
+++ src/cpp/run.sh
@@ -3,7 +3,7 @@
 for class in $(ls *.cpp);
 do
   if [ -f $class ]; then
-    echo "$ g++ ${class} -o ${class}.out" >> ${class.log}
+    echo "$ g++ ${class} -o ${class}.out" >> ${class}.log
     g++ $class -o ${class}.out >> ${class}.log
     echo "$ ./${class}.out" >> ${class}.log
     ./${class}.out >> ${class}.log
